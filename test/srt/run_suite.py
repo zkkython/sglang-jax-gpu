3b1ac634efc893c5d41e39de581e32b5e6d80680
sii-xinglong <253108540219@sii.edu.cn>
2025-08-30 12:17:55 +0800

    use v6e-1 for server test

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -144,18 +144,14 @@ suites = {
         # TestFile("test/srt/test_tpu_availability.py", 3),
     ],
     "per-commit-tpu-v6e-1": [
-        # TestFile("test/srt/test_tpu_availability.py", 3),
-        # TestFile("test/srt/openai_server/basic/test_protocol.py", 10),
-        # TestFile("test/srt/openai_server/basic/test_serving_chat.py", 10),
-        # TestFile("test/srt/openai_server/basic/test_serving_completions.py", 10),
-        # TestFile("test/srt/openai_server/basic/test_openai_server.py", 10),
-    ],
-    "per-commit-tpu-v6e-2": [],
-    "per-commit-tpu-v6e-4": [
+        TestFile("test/srt/test_tpu_availability.py", 3),
         TestFile("test/srt/openai_server/basic/test_protocol.py", 10),
         TestFile("test/srt/openai_server/basic/test_serving_chat.py", 10),
         TestFile("test/srt/openai_server/basic/test_serving_completions.py", 10),
         TestFile("test/srt/openai_server/basic/test_openai_server.py", 10),
+    ],
+    "per-commit-tpu-v6e-2": [],
+    "per-commit-tpu-v6e-4": [
         TestFile("test/srt/test_qwen3_moe_models.py", 45),
         TestFile("test/srt/test_features.py", 30),
         TestFile("test/srt/test_bench_one_batch.py", 15),
