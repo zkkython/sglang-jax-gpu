58efe6aa4ad878c34eeb8a5fba7ef3de988c395b
jimoosciuc <33337387+jimoosciuc@users.noreply.github.com>
2025-10-18 12:54:13 +0800

    fix: mesh context is required when get model (#254)

--- python/sgl_jax/srt/model_loader/loader.py
+++ python/sgl_jax/srt/model_loader/loader.py
@@ -97,11 +97,12 @@ class JAXModelLoader(BaseModelLoader):
         return model_class
 
     def _get_model(self, model_class: Any, model_config: ModelConfig) -> nnx.Module:
-        model = nnx.eval_shape(
-            lambda: model_class(
-                model_config.hf_config, model_config.dtype, self.rng, self.mesh
+        with self.mesh:
+            model = nnx.eval_shape(
+                lambda: model_class(
+                    model_config.hf_config, model_config.dtype, self.rng, self.mesh
+                )
             )
-        )
 
         model.load_weights(model_config, self.rng.default.key.value)
         return model
